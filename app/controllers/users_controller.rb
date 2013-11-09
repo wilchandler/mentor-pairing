756134442b450694ce912a2e80116e3fecd2d782
Dave Hoover <dave.hoover@gmail.com>
2013-11-09 06:50:58 -0600

    Let's not show 'upcoming' appointments from the past

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,8 +6,8 @@ class UsersController < ApplicationController
   def edit
     @user = User.find_by_activation_code(params[:id])
     @availabilities = @user.availabilities.visible.order(:start_time)
-    @menteeing_appointments = @user.menteeing_appointments.order(:start_time)
-    @mentoring_appointments = @user.mentoring_appointments.order(:start_time)
+    @menteeing_appointments = @user.menteeing_appointments.visible.order(:start_time)
+    @mentoring_appointments = @user.mentoring_appointments.visible.order(:start_time)
     @date = params[:month] ? Date.parse(params[:month]) : Date.today
   end
 
