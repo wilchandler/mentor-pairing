c18a53a94e68905e15f51209a821b0e2ec477e2b
Ryan Briones <ryan.briones@brionesandco.com>
2014-02-24 15:43:40 -0800

    Refactored controller logic out for clarity

--- app/controllers/metrics_controller.rb
+++ app/controllers/metrics_controller.rb
@@ -1,7 +1,5 @@
 class MetricsController < ApplicationController
   def weekly
-    @for_week_of = params[:for] ? Time.parse(params[:for]) : Time.now
-    @range = (@for_week_of.beginning_of_week(:sunday)..@for_week_of.end_of_week(:sunday))
-    @total_appointments_count = Appointment.where(:start_time => @range).count
+    @metrics = WeeklyMetricsPresenter.new(params[:for])
   end
 end
---
+++ app/models/weekly_metrics_presenter.rb
@@ -0,0 +1,29 @@
+class WeeklyMetricsPresenter
+  def initialize(for_week_of = nil)
+    @for_week_of = for_week_of
+  end
+
+  def for_week_of
+    if @for_week_of
+      Time.parse(@for_week_of)
+    else
+      Time.now
+    end
+  end
+
+  def starting
+    for_week_of.beginning_of_week(:sunday)
+  end
+
+  def ending
+    for_week_of.end_of_week(:sunday)
+  end
+
+  def total_appointments
+    Appointment.where(:start_time => (starting..ending)).count
+  end
+
+  def has_included_date?(date)
+    (starting..ending).cover?(date)
+  end
+end
