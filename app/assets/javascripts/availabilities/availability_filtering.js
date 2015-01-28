be29d0545b8c311b17f5ee41262441c8f6b67cca
Alyssa Diaz <alyssa@devbootcamp.com>
2015-01-28 09:34:18 -0600

    Added Front end filtering

--- app/assets/javascripts/availabilities/availability_filtering.js
+++ app/assets/javascripts/availabilities/availability_filtering.js
@@ -2,8 +2,14 @@ $(document).ready(function(){
 
 	$("#city-filter").on("change", function(event){
 		var city = $(this).val();
-		$("#availabilities div.panel[data-city='"+city+"']").fadeIn();
-		$("#availabilities div.panel:not(div.panel[data-city='"+city+"'])").fadeOut();
+
+    $("#availabilities div.panel:not(#filter)").fadeOut();
+
+    if(city === "All") {
+      $("#availabilities div.panel:not(#filter)").fadeIn();
+    } else {
+      $("#availabilities div.panel[data-city='"+city+"']").fadeIn();
+    }
 	});
 
 });
