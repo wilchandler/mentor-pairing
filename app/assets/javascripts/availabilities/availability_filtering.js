ba85f85de316f9995fa5d3492c3a29b7221a0018
Alyssa Diaz <alyssa@devbootcamp.com>
2015-02-02 13:18:04 -0600

    Adding panel for when no availabilities for a filter option

--- app/assets/javascripts/availabilities/availability_filtering.js
+++ app/assets/javascripts/availabilities/availability_filtering.js
@@ -3,12 +3,22 @@ $(document).ready(function(){
 	$("#city-filter").on("change", function(event){
 		var city = $(this).val();
 
-    $("#availabilities div.panel:not(#filter)").fadeOut();
+    $("#availabilities div.panel:not(#filter)").hide();
+    $("#no-availability-city").text("");
 
     if(city === "All") {
-      $("#availabilities div.panel:not(#filter)").fadeIn();
+      $("#availabilities div.panel:not(#filter, #no-availability)").fadeIn();
     } else {
-      $("#availabilities div.panel[data-city='"+city+"']").fadeIn();
+
+      var city_availabilities = $("#availabilities div.panel[data-city='"+city+"']");
+
+      if(city_availabilities.length > 0) {
+        $("#availabilities div.panel[data-city='"+city+"']").fadeIn();
+      } else {
+        $("#no-availability-city").text(city);
+        $("#availabilities div.panel[id='no-availability']").fadeIn();
+      }
+
     }
 	});
 
