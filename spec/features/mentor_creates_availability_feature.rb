5328e102531a51836ccfab74d95cbba1b49edc81
Wil Chandler <wilchandler2@gmail.com>
2015-12-03 22:31:08 -0600

    Removes unnecessary Capybara DOM interactions from recurrence preview test

--- spec/features/mentor_creates_availability_feature.rb
+++ spec/features/mentor_creates_availability_feature.rb
@@ -82,14 +82,7 @@ feature "Mentor creates availability" do
     end
 
     it 'displays a preview of the recurring appointment dates', js: true do
-      mentor = FactoryGirl.create(:mentor)
-
       visit new_availability_path
-      fill_in :first_name, with: mentor.first_name
-      fill_in :last_name, with: mentor.last_name
-      fill_in :email, with: mentor.email
-      fill_in :twitter_handle, with: mentor.twitter_handle
-      fill_in :availability_duration, with: 60
 
       mentoring_time = 1.week.from_now
       select_datetime(mentoring_time, :availability_start_time)
